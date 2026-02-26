17ae402243004e1fb00c8c78a008b6758e887680
JKama5 <77468396+JKama5@users.noreply.github.com>
2026-02-25 19:49:01 -0500

    Set curr pose as home!!

--- CTR_Educational_Platform-main/test.m
+++ CTR_Educational_Platform-main/test.m
@@ -3,21 +3,26 @@ clear; clc;
 
 startPose = Pose(0, 0, 0, 0, 0, 0);
 drive_bot = Drive(startPose);
+% dist = -10;
+
+% drive_bot.travel_for(dist,dist,dist,0,0,0)
+drive_bot.set_current_pose_as_home();
 
 %% Test linear 1
-drive_bot.travel_for(0,0,10,0,0,0)
+% drive_bot.travel_for(0,0,1,0,0,0)
 
 %% Test linear 2
-drive_bot.travel_for(0,10,0,0,0,0)
-
-%% Test linear 3
-drive_bot.travel_for(0,0,10,0,0,0)  
-
-%% Test rotation 1
-drive_bot.travel_for(0,0,0,90,0,0)
-
-%% Test rotation 2
-drive_bot.travel_for(0,0,0,0,90,0)
-
-%% Test rotation 3
-drive_bot.travel_for(0,0,0,0,0,90)
+% drive_bot.travel_for(0,-1,0,0,0,0)
+% 
+%drive_bot.travel_for(20,0,0,0,0,0)
+% %% Test linear 3
+% drive_bot.travel_for(0,0,10,0,0,0)  
+% 
+% %% Test rotation 1
+% drive_bot.travel_for(0,0,0,-5,0,0)
+% 
+% %% Test rotation 2
+% drive_bot.travel_for(0,0,0,0,-10,0)
+% 
+% %% Test rotation 3
+% drive_bot.travel_for(0,0,0,0,0,90)
